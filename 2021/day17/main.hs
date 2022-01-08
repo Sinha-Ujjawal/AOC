a2e4f9cf528b98e178c4911bce2f60e336283f90
Sinha-Ujjawal <https://github.com/Sinha-Ujjawal>
2022-01-09 02:24:15 +0530

    Add 2021 Day 17 Part 2.. Phew

--- 2021/day17/main.hs
+++ 2021/day17/main.hs
@@ -2,6 +2,10 @@
 {-# LANGUAGE ViewPatterns #-}
 
 import Control.Monad (forM_)
+import Data.List (sortOn)
+import qualified Data.Map as M
+import Data.Maybe (catMaybes)
+import qualified Data.Set as S
 import qualified Data.Text as T
 import System.IO (hFlush, stdout)
 import Text.Read (readMaybe)
@@ -11,6 +15,12 @@ prompt text = do
   putStr text
   hFlush stdout
   getLine
+
+takeWhileWithThreshold :: Int -> (a -> Bool) -> [a] -> [a]
+takeWhileWithThreshold thresh match = go 0
+  where
+    go t [] = []
+    go t (x : xs) = let b = match x in if b || t < thresh then x : go (if b then t else t + 1) xs else []
 
 readMaybeText :: Read a => T.Text -> Maybe a
 readMaybeText = readMaybe . T.unpack
@@ -27,19 +37,72 @@ parseInput = go . T.pack
     go'' [Just xl, Just xu] [Just yl, Just yu] = Just ((xl, xu), (yl, yu))
     go'' _ _ = Nothing
 
-findMaxHeight :: (Int, Int) -> Int
-findMaxHeight (yl, yu) = (n * (n + 1)) `div` 2
+findMaxVelocity :: Int -> Int -> Int
+findMaxVelocity yl yu = max (go yl) (go yu)
   where
-    n = max (go yl) (go yu)
-
     go y
       | y < 0 = -y - 1
       | otherwise = y
+
+findMaxHeight :: (Int, Int) -> Int
+findMaxHeight (yl, yu) = (n * (n + 1)) `div` 2
+  where
+    n = findMaxVelocity yl yu
+
+findTimes :: Int -> Int -> [Double]
+findTimes u s = filter (>= 0) $ filter (not . isNaN) [(a + b) / 2, (a - b) / 2]
+  where
+    a = fromIntegral $ 1 + u + u
+    b = sqrt (fromIntegral $ 1 + 4 * ((u * u) + u - s - s))
+
+findTime :: Int -> Int -> Maybe Int
+findTime u s =
+  if null ts then Nothing else Just $ floor $ minimum ts
+  where
+    ts = findTimes u s
+
+findDisplacement :: Int -> Int -> Int
+findDisplacement u t = (u * t) - ((t * (t - 1)) `div` 2)
+
+findReachableDisplacements :: Int -> Int -> Int -> M.Map Int Int
+findReachableDisplacements s1 s2 u
+  | s1 > s2 = findReachableDisplacements s2 s1 u
+  | otherwise =
+    case ts of
+      [] -> M.empty
+      _ ->
+        M.fromList $
+          filter (\(_, s) -> (s >= s1) && (s <= s2)) $
+            takeWhileWithThreshold 10 (\(_, s) -> (s >= s1) && (s <= s2)) $
+              (\t -> (t, findDisplacement u t))
+                <$> [minimum ts ..]
+  where
+    ts = catMaybes (findTime u <$> [s1, s2])
 
 solvePart1 :: String -> Maybe Int
 solvePart1 input = do
   ((_, _), (yl, yu)) <- parseInput input
   pure $ findMaxHeight (yl, yu)
+
+isCompatible :: ((Int, Int), (Int, Int)) -> (Int, Int) -> Bool
+isCompatible ((xl, xu), (yl, yu)) (xv, yv) =
+  (not . null $ S.intersection txs tys) || (not (null xs) && (Just (xv + 1) <= S.lookupMax tys) && (d >= xl) && (d <= xu))
+  where
+    xs = M.filterWithKey (\t _ -> t <= (xv + 1)) $ findReachableDisplacements xl xu xv
+    d = findDisplacement xv (xv + 1)
+    txs = S.fromList . M.keys $ xs
+    tys = S.fromList . M.keys $ findReachableDisplacements yl yu yv
+
+solvePart2 :: String -> Maybe String
+solvePart2 input = do
+  ((xl, xu), (yl, yu)) <- parseInput input
+  let xvs = findVelocities xl xu
+      yvs = findVelocities yl yu
+  pure $ show $ length [show xv ++ "," ++ show yv | xv <- xvs, yv <- yvs, isCompatible ((xl, xu), (yl, yu)) (xv, yv)]
+  where
+    findVelocities l u = [((-v) - 1) .. (v + 1)]
+      where
+        v = abs $ findMaxVelocity l u
 
 main :: IO ()
 main = do
@@ -48,3 +111,8 @@ main = do
 
   putStrLn "Part 1:"
   forM_ ls (\l -> putStrLn $ l ++ " => " ++ show (solvePart1 l))
+
+  putStrLn ""
+
+  putStrLn "Part 2:"
+  forM_ ls (\l -> putStrLn $ l ++ " => " ++ show (solvePart2 l))
