6f910ecd710ea10a8b27f95cc6e5e861990bfd65
Sinha-Ujjawal <ujjawalsinhacool16021998@gmail.com>
2022-11-02 00:21:39 +0530

    Add Day6 2015 - Haskell Solution using Mutable arrays

--- 2015/day6/day6-haskell/app/Main.hs
+++ 2015/day6/day6-haskell/app/Main.hs
@@ -1,12 +1,15 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RankNTypes #-}
 {-# LANGUAGE ViewPatterns #-}
 
 module Main where
 
+import Control.Monad (forM_)
+import qualified Data.Array.Base as AIO
+import qualified Data.Array.IO as AIO
+import Data.Bits (xor)
 import Data.Maybe (fromMaybe)
 import qualified Data.Text as T
-import Data.Vector ((!), (//))
-import qualified Data.Vector as V
 import System.IO (hFlush, stdout)
 import qualified Text.Read as T
 
@@ -22,27 +25,24 @@ type Box = (Coordinate, Coordinate)
 
 type Brightness = Int
 
-type LightGrid = V.Vector Brightness
+type LightGrid = AIO.IOArray Int Int
 
-initLights :: LightGrid
-initLights = V.generate (1000 * 1000) (const 0)
-
-coordToIdx :: Coordinate -> Int
-coordToIdx (row, col) = row * 1000 + col
+initLights :: IO LightGrid
+initLights = AIO.newArray (0, 1000 * 1000) 0
 
 boxCoordinates :: Box -> [Coordinate]
 boxCoordinates ((xl, yl), (xu, yu)) =
   [(x, y) | x <- [xl .. xu], y <- [yl .. yu]]
 
-type BrightnessUpdateFn = (Coordinate -> Brightness -> Brightness)
+type BrightnessUpdateFn = (Brightness -> Brightness)
 
-updateGrid :: BrightnessUpdateFn -> Box -> LightGrid -> LightGrid
-updateGrid updateFn box grid =
-  grid
-    `V.unsafeUpd` [ (idx, max (updateFn coord (grid ! idx)) 0)
-                    | coord <- boxCoordinates box,
-                      idx <- [coordToIdx coord]
-                  ]
+updateGrid :: BrightnessUpdateFn -> Box -> LightGrid -> IO LightGrid
+updateGrid updateFn box grid = do
+  forM_ (boxCoordinates box) $ \(row, col) -> do
+    let idx = row * 1000 + col
+    oldValue <- AIO.readArray grid idx
+    AIO.writeArray grid idx (max 0 (updateFn oldValue))
+  return grid
 
 data Instruction = TurnOn !Box | TurnOff !Box | Toggle !Box deriving (Show, Eq)
 
@@ -69,33 +69,35 @@ parseLine _ = Nothing
 parseLines :: [T.Text] -> Maybe [Instruction]
 parseLines = traverse parseLine
 
-solvePart1 :: [Instruction] -> Int
-solvePart1 = V.length . V.filter (> 0) . foldr applyInstruction initLights . reverse
+solve :: (BrightnessUpdateFn, BrightnessUpdateFn, BrightnessUpdateFn) -> [Instruction] -> IO LightGrid
+solve (turnOnUpdateFn, turnOffUpdateFn, toggleUpdateFn) instructions = do
+  grid <- initLights
+  forM_ instructions $ \instruction -> do
+    case instruction of
+      TurnOn box -> updateGrid turnOnUpdateFn box grid
+      TurnOff box -> updateGrid turnOffUpdateFn box grid
+      Toggle box -> updateGrid toggleUpdateFn box grid
+  return grid
+
+solvePart1 :: [Instruction] -> IO Int
+solvePart1 instructions = do
+  grid <- solve (turnOnUpdateFn, turnOffUpdateFn, toggleUpdateFn) instructions
+  elems <- AIO.getElems grid
+  return . length $ filter (> 0) elems
   where
-    applyInstruction :: Instruction -> LightGrid -> LightGrid
-    applyInstruction (TurnOn box) = turnOn box
-    applyInstruction (TurnOff box) = turnOff box
-    applyInstruction (Toggle box) = toggle box
+    turnOnUpdateFn = const 1
+    turnOffUpdateFn = const 0
+    toggleUpdateFn b = b `xor` 1
 
-    turnOn = updateGrid (const (const 1))
-    turnOff = updateGrid (const (const 0))
-    toggle box grid = updateGrid go box grid
-      where
-        go coord _
-          | grid ! coordToIdx coord == 1 = 0
-          | otherwise = 1
-
-solvePart2 :: [Instruction] -> Int
-solvePart2 = V.sum . foldr applyInstruction initLights . reverse
+solvePart2 :: [Instruction] -> IO Int
+solvePart2 instructions = do
+  grid <- solve (turnOnUpdateFn, turnOffUpdateFn, toggleUpdateFn) instructions
+  elems <- AIO.getElems grid
+  return . sum $ filter (> 0) elems
   where
-    applyInstruction :: Instruction -> LightGrid -> LightGrid
-    applyInstruction (TurnOn box) = turnOn box
-    applyInstruction (TurnOff box) = turnOff box
-    applyInstruction (Toggle box) = toggle box
-
-    turnOn = updateGrid (const (+ 1))
-    turnOff = updateGrid (const (\x -> x - 1))
-    toggle = updateGrid (const (+ 2))
+    turnOnUpdateFn = (+) 1
+    turnOffUpdateFn b = if b > 0 then b - 1 else 0
+    toggleUpdateFn = (+) 2
 
 main :: IO ()
 main = do
@@ -103,8 +105,8 @@ main = do
   fileLines <- T.lines . T.pack <$> readFile filename
 
   let instructions = fromMaybe [] $ parseLines fileLines
-      part1Ans = solvePart1 instructions
-      part2Ans = solvePart2 instructions
+  part1Ans <- solvePart1 instructions
+  part2Ans <- solvePart2 instructions
 
   putStrLn $ "Part 1: " ++ show part1Ans
   putStrLn $ "Part 2: " ++ show part2Ans
