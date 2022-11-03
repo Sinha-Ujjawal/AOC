f15d8f431be7b826dea452f44c099d7d5840ae63
Sinha-Ujjawal <ujjawalsinhacool16021998@gmail.com>
2022-11-03 22:18:02 +0530

    2015 Day6 Clojure - Using Arrays

--- 2015/day6/day6-clojure/src/day6_clojure/core.clj
+++ 2015/day6/day6-clojure/src/day6_clojure/core.clj
@@ -2,73 +2,80 @@
   (:gen-class)
   (:require [clojure.string :as string]))
 
+(set! *warn-on-reflection* true)
+(set! *unchecked-math* :warn-on-boxed)
+
+(def ^:const grid-size 1000)
+
 (defn init-light-grid []
-  (transient (vec (repeat (* 1000 1000) 0))))
+  (long-array (* grid-size grid-size)))
 
+(defn update-grid! [update-fn
+                    [^long rowl ^long coll ^long rowu ^long colu]
+                    ^longs grid]
+  (doseq [^long row (range rowl (inc rowu))
+          ^long col (range coll (inc colu))]
+    (let [idx (+ (* grid-size row) col)]
+      (aset grid idx (Math/max 0 ^long (update-fn ^long (aget grid idx)))))))
 
-(defn update-grid [update-fn [rowl coll rowu colu] grid]
-  (loop [row rowl col coll grid grid]
-    (let [idx (+ (* row 1000) col)]
-      (if (and (<= row rowu) (<= col colu))
-        (recur
-         (+ row (if (= col colu) 1 0))
-         (if (= col colu) coll (inc col))
-         (assoc! grid idx (max 0 (update-fn (grid idx)))))
-        grid))))
-
-(defn apply-instruction [turn-on-update-fn
-                         turn-off-update-fn
-                         toggle-update-fn
-                         grid [instruction-type box]]
+(defn apply-instruction! [turn-on-update-fn
+                          turn-off-update-fn
+                          toggle-update-fn
+                          ^longs grid
+                          [instruction-type box]]
   (cond
-    (= instruction-type :turn-on) (update-grid turn-on-update-fn box grid)
-    (= instruction-type :turn-off) (update-grid turn-off-update-fn box grid)
-    (= instruction-type :toggle) (update-grid toggle-update-fn box grid)
+    (= instruction-type :turn-on) (update-grid! turn-on-update-fn box grid)
+    (= instruction-type :turn-off) (update-grid! turn-off-update-fn box grid)
+    (= instruction-type :toggle) (update-grid! toggle-update-fn box grid)
     :else grid))
 
-(defn solve [turn-on-update-fn
-             turn-off-update-fn
-             toggle-update-fn
+(defn solve [turn-on-update-fn!
+             turn-off-update-fn!
+             toggle-update-fn!
              instructions]
-  (let [reducer (partial apply-instruction turn-on-update-fn turn-off-update-fn toggle-update-fn)]
-    (loop [instructions instructions grid (init-light-grid)]
-      (if (empty? instructions) (persistent! grid)
-          (recur (rest instructions) (reducer grid (first instructions)))))))
+  (let [^longs grid (init-light-grid)]
+    (doseq [instruction instructions]
+      (apply-instruction! turn-on-update-fn!
+                          turn-off-update-fn!
+                          toggle-update-fn!
+                          grid
+                          instruction))
+    (vec grid)))
 
 (defn solve-part-1 [instructions]
   (let [turn-on-update-fn (fn [_] 1)
         turn-off-update-fn (fn [_] 0)
-        toggle-update-fn (fn [b] (bit-xor b 1))]
+        toggle-update-fn (fn [^long b] (if (zero? b) 1 0))]
     (->> instructions
          (solve turn-on-update-fn turn-off-update-fn toggle-update-fn)
-         (filter #(> % 0))
+         (filter (fn [^long x] (> x 0)))
          (count))))
 
 (defn solve-part-2 [instructions]
   (let [turn-on-update-fn inc
         turn-off-update-fn dec
-        toggle-update-fn #(+ 2 %)]
+        toggle-update-fn (fn [^long x] (+ x 2))]
     (->> instructions
          (solve turn-on-update-fn turn-off-update-fn toggle-update-fn)
-         (filter #(> % 0))
+         (filter (fn [^long x] (> x 0)))
          (apply +))))
 
 (defn parse-turn-on [line]
   (let [xs (rest (re-find #"turn on (\d+)\,(\d+) through (\d+)\,(\d+)" line))]
     (if (= 4 (count xs))
-      [:turn-on (mapv #(Integer/parseInt %) xs)]
+      [:turn-on (mapv #(Long/parseLong %) xs)]
       nil)))
 
 (defn parse-turn-off [line]
   (let [xs (rest (re-find #"turn off (\d+)\,(\d+) through (\d+)\,(\d+)" line))]
     (if (= 4 (count xs))
-      [:turn-off (mapv #(Integer/parseInt %) xs)]
+      [:turn-off (mapv #(Long/parseLong %) xs)]
       nil)))
 
 (defn parse-toggle [line]
   (let [xs (rest (re-find #"toggle (\d+)\,(\d+) through (\d+)\,(\d+)" line))]
     (if (= 4 (count xs))
-      [:toggle (mapv #(Integer/parseInt %) xs)]
+      [:toggle (mapv #(Long/parseLong %) xs)]
       nil)))
 
 (defn parse-line [line]
